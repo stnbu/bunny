499351972d5ade5672c064323a669e189c0a2ca5
Mike Burr <mb@unintuitive.org>
2023-11-01 10:19:00 -0600

    1

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,9 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, rotator_system)
         .run();
 }
 
-/// this component indicates what entities should rotate
-#[derive(Component)]
-struct Rotator;
-
-/// rotates the parent, which will result in the child also rotating
-fn rotator_system(time: Res<Time>, mut query: Query<&mut Transform, With<Rotator>>) {
-    for mut transform in &mut query {
-        transform.rotate_x(3.0 * time.delta_seconds());
-    }
-}
-
-/// set up a simple scene with a "parent" cube and a "child" cube
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -31,32 +18,18 @@ fn setup(
         ..default()
     });
 
-    // parent cube
-    commands
-        .spawn((
-            PbrBundle {
-                mesh: cube_handle.clone(),
-                material: cube_material_handle.clone(),
-                transform: Transform::from_xyz(0.0, 0.0, 1.0),
-                ..default()
-            },
-            Rotator,
-        ))
-        .with_children(|parent| {
-            // child cube
-            parent.spawn(PbrBundle {
-                mesh: cube_handle,
-                material: cube_material_handle,
-                transform: Transform::from_xyz(0.0, 0.0, 3.0),
-                ..default()
-            });
-        });
-    // light
+    commands.spawn(PbrBundle {
+        mesh: cube_handle.clone(),
+        material: cube_material_handle.clone(),
+        transform: Transform::from_xyz(0.0, 0.0, 1.0),
+        ..default()
+    });
+
     commands.spawn(PointLightBundle {
         transform: Transform::from_xyz(4.0, 5.0, -4.0),
         ..default()
     });
-    // camera
+
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(5.0, 10.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
