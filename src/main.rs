d304deb352df597f1551c0aba69098d164b8513a
Mike Burr <mb@unintuitive.org>
2023-11-01 10:45:19 -0600

    .

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,44 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::UVSphere {
-            radius: 1.0 / 40.0,
-            ..Default::default()
-        })),
-        material: materials.add(StandardMaterial {
-            base_color: Color::GREEN,
-            ..default()
-        }),
-        ..default()
-    });
+    let points = Vec::from([
+        Vec3::new(1.0, 1.0, 1.0),
+        Vec3::new(1.0, 1.0, 1.0),
+        Vec3::new(1.0, 1.0, 1.0),
+        Vec3::new(1.0, 1.0, 1.0),
+        Vec3::new(1.0, 1.0, 1.0),
+        Vec3::new(1.0, 1.0, 1.0),
+        Vec3::new(1.0, 1.0, 1.0),
+        Vec3::new(1.0, 1.0, 1.0),
+        Vec3::new(1.0, 1.0, 1.0),
+    ]);
 
+    let v0 = Vec3::new(0.0, -0.5, 0.0);
+    let v1 = Vec3::new(0.0, 0.5, 0.0);
+    let v2 = Vec3::ZERO;
+    let up = Vec3::Z;
+
+    let mut vertices: Vec<Vec3> = Vec::new();
+
+    for point in points {
+        vertices.push(v0);
+        vertices.push(v1);
+    }
+
+    for vertex in vertices {
+        commands.spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::UVSphere {
+                radius: 1.0 / 40.0,
+                ..Default::default()
+            })),
+            material: materials.add(StandardMaterial {
+                base_color: Color::GREEN,
+                ..default()
+            }),
+            transform: Transform::from_translation(vertex),
+            ..default()
+        });
+    }
     commands.spawn(PointLightBundle {
         transform: Transform::from_xyz(4.0, 5.0, -4.0),
         ..default()
